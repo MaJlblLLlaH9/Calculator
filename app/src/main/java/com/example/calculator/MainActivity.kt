3f7ebd956aed9dca6b7285cd84d4a579b9221137
MaJlblLLlaH9 <ilya.melchenko@mail.ru>
2022-04-11 15:43:33 +0300

    Set math operation

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -10,81 +10,129 @@ class MainActivity : AppCompatActivity() {
     //   val conditionText1
 // lateinint - что это ? Какой есть другой способ инициализации
 
-    fun setConditionField(str:String){
+    fun setConditionField(str: String) {
         val conditionText = findViewById<TextView>(R.id.condition)
-        conditionText.setText(conditionText.text.toString()+str)
+        conditionText.setText(conditionText.text.toString() + str)
     }
 
     fun zero() {
-        findViewById<TextView>(R.id.zero).setOnClickListener{
-            Toast.makeText(it.context, "0 is added",Toast.LENGTH_SHORT).show()
+        findViewById<TextView>(R.id.zero).setOnClickListener {
+            Toast.makeText(it.context, "0 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.zero).text.toString())
         }
     }
+
     fun threeZero() {
-        findViewById<TextView>(R.id.threeZero).setOnClickListener{
+        findViewById<TextView>(R.id.threeZero).setOnClickListener {
             Toast.makeText(it.context, "000 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.threeZero).text.toString())
         }
     }
-    fun one(){
-            findViewById<TextView>(R.id.one).setOnClickListener{
-                Toast.makeText(it.context, "1 is added", Toast.LENGTH_SHORT).show()
-                setConditionField(findViewById<TextView>(R.id.one).text.toString())
+
+    fun one() {
+        findViewById<TextView>(R.id.one).setOnClickListener {
+            Toast.makeText(it.context, "1 is added", Toast.LENGTH_SHORT).show()
+            setConditionField(findViewById<TextView>(R.id.one).text.toString())
         }
     }
+
     fun two() {
-        findViewById<TextView>(R.id.two).setOnClickListener{
+        findViewById<TextView>(R.id.two).setOnClickListener {
             Toast.makeText(it.context, "2 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.two).text.toString())
         }
     }
+
     fun three() {
-        findViewById<TextView>(R.id.three).setOnClickListener{
+        findViewById<TextView>(R.id.three).setOnClickListener {
             Toast.makeText(it.context, "3 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.three).text.toString())
         }
     }
+
     fun four() {
-        findViewById<TextView>(R.id.four).setOnClickListener{
+        findViewById<TextView>(R.id.four).setOnClickListener {
             Toast.makeText(it.context, "4 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.four).text.toString())
         }
     }
+
     fun five() {
-        findViewById<TextView>(R.id.five).setOnClickListener{
+        findViewById<TextView>(R.id.five).setOnClickListener {
             Toast.makeText(it.context, "5 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.five).text.toString())
         }
     }
+
     fun six() {
-        findViewById<TextView>(R.id.six).setOnClickListener{
+        findViewById<TextView>(R.id.six).setOnClickListener {
             Toast.makeText(it.context, "6 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.six).text.toString())
         }
     }
+
     fun seven() {
-        findViewById<TextView>(R.id.seven).setOnClickListener{
+        findViewById<TextView>(R.id.seven).setOnClickListener {
             Toast.makeText(it.context, "7 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.seven).text.toString())
         }
     }
+
     fun eight() {
-        findViewById<TextView>(R.id.eight).setOnClickListener{
+        findViewById<TextView>(R.id.eight).setOnClickListener {
             Toast.makeText(it.context, "8 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.eight).text.toString())
         }
     }
-    fun nine(){
-        findViewById<TextView>(R.id.nine).setOnClickListener{
+
+    fun nine() {
+        findViewById<TextView>(R.id.nine).setOnClickListener {
             Toast.makeText(it.context, "9 is added", Toast.LENGTH_SHORT).show()
             setConditionField(findViewById<TextView>(R.id.nine).text.toString())
         }
     }
-    fun equall(){
+
+    fun plus() {
+        findViewById<TextView>(R.id.plus).setOnClickListener {
+            setConditionField(findViewById<TextView>(R.id.plus).text.toString())
+        }
+    }
+
+    fun minus() {
+        findViewById<TextView>(R.id.minus).setOnClickListener {
+            setConditionField(findViewById<TextView>(R.id.minus).text.toString())
+        }
+    }
+
+    fun multiply() {
+        findViewById<TextView>(R.id.multiply).setOnClickListener {
+            setConditionField(findViewById<TextView>(R.id.multiply).text.toString())
+        }
+    }
+
+    fun divide() {
+        findViewById<TextView>(R.id.divide).setOnClickListener {
+            setConditionField(findViewById<TextView>(R.id.divide).text.toString())
+        }
+    }
+
+    fun percent() {
+        findViewById<TextView>(R.id.percent)
+    }
+
+    fun equall() {
         findViewById<TextView>(R.id.equall)
     }
 
+    fun clear() {
+        findViewById<TextView>(R.id.clear).setOnClickListener{
+            val condition = findViewById<TextView>(R.id.condition)
+            val conditionString = condition.text.toString()
+            if(conditionString.isNotEmpty())   {
+                condition.text = conditionString.substring(0,conditionString.length -1)
+                }
+            }
+    }
     fun reset(){
             findViewById<TextView>(R.id.reset).setOnClickListener{
             Toast.makeText(this, "Clear", Toast.LENGTH_LONG).show()
@@ -104,7 +152,10 @@ class MainActivity : AppCompatActivity() {
         eight()
         nine()
         reset()
-
+        plus()
+        minus()
+        multiply()
+        divide()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
